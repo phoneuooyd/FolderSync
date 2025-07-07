98b1ac152e062e5816573d2db3a4d256da0b91ce
Przemysław Przybyszewski <103174443+phoneuooyd@users.noreply.github.com>
2025-07-07 16:21:11 +0200

    File management commands (#1)
    added a interface IFileCommand, Copy and Delete commands, and a file comparer (class + interface)

---
+++ Commands/IFileCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSync.Commands
+{
+    public interface IFileCommand
+    {
+        void Execute();
+    }
+}
